ac9fd7f07085ca6c205a352daff6390b258760f3
Sanghwa Song <cnrrn421@gmail.com>
2024-04-08 20:00:34 +0900

    [Style] 변수명 수정

--- Assets/Scripts/Enemy/Triangle.cs
+++ Assets/Scripts/Enemy/Triangle.cs
@@ -11,28 +11,28 @@ using Utilities;
 public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
 {
 
-    [SerializeField] protected Transform target;
-    [SerializeField] protected RayBox ray;
-    [SerializeField] float jumpPower;
+    [SerializeField] protected Transform _target;
+    [SerializeField] protected RayBox _ray;
+    [SerializeField] float _jumpForce;
     
     [Header("Dash")]
-    [SerializeField] float dashForce;
-    [SerializeField] float dashDistance;
+    [SerializeField] float _dashForce;
+    [SerializeField] float _dashDistance;
 
     // public enum EnemyType { Civilian, Soldier, Jumper }
     // public EnemyType enemyType;
-    [SerializeField] GameObject hitBox;
-    protected Vector2 moveDir;
+    [SerializeField] GameObject _hitBox;
+    protected Vector2 _moveDir;
 
     void Start()
     {
-        hp = maxHp;
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        enemyState = EnemyState.Idle;
-        moveDir = Vector2.left;
+        _hp = _maxHp;
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _enemyState = EnemyState.Idle;
+        _moveDir = Vector2.left;
         StartCoroutine(EnemyStateHandler());
-        EnemyManager.instance.AddEnemy(gameObject);
+        EnemyManager.Instance.Add(this);
     }
 
     //매 프레임마다 확인해야 할 것? bool 애니메이션은 애니메이션 함수로 따로 처리하기 파편화되어있으니 보기 싫다
@@ -42,110 +42,70 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
         TriangleAnim();
     }
 
-    protected override IEnumerator Idle()
-    {
+    //triangle 기사는 가만히 있는 컨셉(근위병 같은 느낌?)
+    protected override IEnumerator Idle()   { yield return null; }
+    protected override IEnumerator Detect() { yield return null; }
+    protected override IEnumerator Attack() { yield return null; }
+    protected override IEnumerator Die()    { yield return null; }
 
-        /*
-            case EnemyType.Jumper:
-                isMoving = true;
-                Move();
-                // [TG] [2024-04-06] [Refactor]
-                // 1. Raybox의 CheckWithRay에 시작 위치 매개변수를 추가하였으므로 같이 수정 
-                // 2. 기존 Raybox의 trasnform.position이 (-0.5, 0, 0) 이고 BoxCollider의 Offset이 (0, 0) 임을 반영하여 변경
-                // 3. 기존 값을 확인하는 것은 비효율적이기 때문에 참조를 할 수 있게 수정이 필요해 보임
-                if (!ray.CheckWithRay(transform.position + new Vector3(-0.5f, 0, 0), Vector2.down, 5)
-                    || ray.CheckWithRay(transform.position + new Vector3(-0.5f, 0, 0), moveDir, .5f))
-                {
-                    isMoving = false;
-                    yield return new WaitForSeconds(2f);
-                    TurnAround();
-                    isMoving = true;
-                }
-                yield break;
-        */
-        
-        //triangle 기사는 가만히 있는 컨셉(근위병 같은 느낌?)
-
-        yield return null;
-    }
-
-    protected override IEnumerator Detect()
-    {
-        yield return null;
-    }
-
-    protected override IEnumerator Attack()
-    {   
-        yield return null;  
-    }   
-
-    protected override IEnumerator Die()
-    {
-        throw new System.NotImplementedException();
-    }
-    
-    protected virtual void TriangleAnim()
-    {
-        
-    }
+    protected virtual void TriangleAnim()   { return; }
 
     protected void Move()
     {   
-        Vector2 newPos = (Vector2)transform.position + (moveDir * moveSpd * Time.deltaTime);
+        Vector2 newPos = (Vector2)transform.position + (_moveDir * _moveSpd * Time.deltaTime);
         transform.position = newPos;
 
-        transform.localScale = (moveDir == Vector2.left) ? new Vector2(1, 1) : new Vector2(-1, 1);
+        transform.localScale = (_moveDir == Vector2.left) ? new Vector2(1, 1) : new Vector2(-1, 1);
     }
 
     protected void TurnAround()
     {
-        moveDir = -moveDir;
+        _moveDir = -_moveDir;
         transform.localScale = new Vector2(-transform.localScale.x,transform.localScale.y);
     }
 
     protected IEnumerator Jump()
     {
-        var jumpDir = new Vector2(-runDir.x, 1);
-        rb.AddForce(jumpDir * jumpPower, ForceMode2D.Impulse);
+        Vector2 jumpDir = new Vector2(-runDir.x, 1);
+        _rb.AddForce(jumpDir * _jumpForce, ForceMode2D.Impulse);
 
         yield return new WaitForSeconds(1f);
 
         StateChange(EnemyState.Detect);
     }
 
-    protected IEnumerator Dash(Vector2 vector, float dashForce)
+    protected IEnumerator Dash(Vector2 vector, float _dashForce)
     {
-        if (dashForce < Mathf.Epsilon) yield break;
+        if (_dashForce < Mathf.Epsilon) yield break;
         
-        rb.AddForce(vector.normalized * dashForce, ForceMode2D.Impulse);
-        yield return new WaitForSeconds(vector.magnitude / dashForce);
-        rb.velocity = Vector2.zero;
-        yield break;
-    }   
+        _rb.AddForce(vector.normalized * _dashForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(vector.magnitude / _dashForce);
+        _rb.velocity = Vector2.zero;
+    }
 
     protected IEnumerator AttackCombo()
     {
         
-        anim.SetTrigger("Attack");
+        _anim.SetTrigger("Attack");
 
         //애니메이션과 이동 맞추기 위해 시간 조정
         yield return new WaitForSeconds(.2f);
 
         //공격할때 앞으로 돌진하는 모션
-        yield return Dash(-runDir * dashDistance, dashForce);
+        yield return Dash(-runDir * _dashDistance, _dashForce);
 
         yield return new WaitForSeconds(.3f);
         
         //target이 null이 아니고 아직도 범위 안에 있으면 2타 실행
-        if (target && TargetDistance(target) < meeleeRange)
+        if (_target && TargetDistance(_target) < _meeleeRange)
         {
-            anim.SetTrigger("Attack");
+            _anim.SetTrigger("Attack");
 
             //애니메이션과 이동 맞추기 위해 시간 조정
             yield return new WaitForSeconds(.25f);
 
             //공격할때 앞으로 돌진하는 모션
-            yield return Dash(-runDir * dashDistance * 0.5f, dashForce * 0.5f);
+            yield return Dash(-runDir * _dashDistance * 0.5f, _dashForce * 0.5f);
 
             this.Log("attack2");
         }
@@ -153,15 +113,14 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
         //공격패턴 끝나면 다시 감지
         StateChange(EnemyState.Detect);
     }
-    
 
     protected void CalculateDir()
     {
-        if (!target) return;
+        if (!_target) return;
         //방향 설정
-        detectPoint = target.position;
-        var curPoint = (Vector2)transform.position;
-        var dir = curPoint.x - detectPoint.x > 0;
+        _detectPoint = _target.position;
+        Vector2 curPoint = (Vector2)transform.position;
+        bool dir = curPoint.x - _detectPoint.x > 0;
         runDir = dir ? new Vector2(1, 0) : new Vector2(-1, 0);
 
         transform.localScale = dir ? new Vector2(1, 1) : new Vector2(-1, 1);
@@ -169,9 +128,8 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
    
     protected float TargetDistance(Transform target)
     {
-        
         float dis = Vector2.Distance(transform.position, target.position);
-        isMeeleeRange = dis < meeleeRange;
+        _isMeeleeRange = dis < _meeleeRange;
         return dis;
     }
 
@@ -183,12 +141,12 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
         if (target != null)
         {
             //매개변수 받은 놈으로 초기화시켜주고
-            this.target = target;
+            this._target = target;
 
             //방향 설정
-            detectPoint = target.position;
-            var curPoint = (Vector2)transform.position;
-            var dir = curPoint.x - detectPoint.x > 0;
+            _detectPoint = target.position;
+            Vector2 curPoint = (Vector2)transform.position;
+            bool dir = curPoint.x - _detectPoint.x > 0;
             runDir = dir ? new Vector2(1, 0) : new Vector2(-1, 0);
 
             //state 바꾸기
@@ -197,9 +155,9 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
 
         else
         {
-            this.target = target;
-            isMeeleeRange = false;
-            this.Log($"{target} Out of Sight");
+            this.Log($"{this._target} Out of Sight");
+            this._target = null;
+            _isMeeleeRange = false;
             //state 바꾸기
             StateChange(EnemyState.Idle);
         }    
@@ -228,9 +186,9 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
         disArmCnt++;
         if (disArmCnt == level)
         {
-            hitBox.SetActive(false);
+            _hitBox.SetActive(false);
             //enemyType = EnemyType.Civilian;
-            EnemyManager.instance.ChangeEnemy(gameObject, "Civilian");
+            EnemyManager.Instance.Replace(this, "Civilian");
         }
     }
 
@@ -260,20 +218,15 @@ public class Triangle : Enemy, IAttackable, IDetectable, IDamageable
         //코루틴 최상위에서 hp가 0이 되면 자동으로 Death 코루틴이 실행되도록 했으나, Attack 하위 코루틴이 실행될때는 코드 흐름 중지,
         //최상위 코루틴은 딜레이되어서 0.n초간 딜레이가 있음;
         //
-        hp -= dmg;
-        this.Log($"currentHp : {hp} - {dmg} = {hp-dmg}");
+        this.Log($"currentHp : {_hp} - {dmg} = {_hp - dmg}");
+        _hp -= dmg;
         
-        if (hp<=0)
-        {
-            //Death effect
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            //blood effect?
-            //dosth
-        }
+        //blood effect?
+        //dosth
 
+        //Death effect는 enemy.Die()에서 처리
+        //enemy.Die()에서 EnemyManager를 통해 Destroy가 진행된다
+        if (_hp <= 0) { StateChange(EnemyState.Die); }
     }
 
 }
