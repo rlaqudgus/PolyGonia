36acc340a9689f7acc8004dc63576fb34526ab1d
Sanghwa Song <cnrrn421@gmail.com>
2024-04-15 00:00:24 +0900

    [Feature] UIManager 작성
    UI를 켜고 끄는 EnableUI, DisableUI 함수 작성
    Pause, 사운드 세팅 UI 추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static UIManager _instance;
+    public  static UIManager  Instance { get { return _instance; } }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject[] userInterface;
+    private Dictionary<string, GameObject> _uiDict = new Dictionary<string, GameObject>();
+
+    public GameObject pauseUI;
+
+    public Slider MusicVolumeController;
+    public Slider SFXVolumeController;
+
+    private void Awake()
+    {   
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }   
+
+    void Start()    
+    {   
+        foreach (GameObject ui in userInterface) 
+        {
+            _uiDict.Add(ui.name, ui);
+        }
+
+        MusicVolumeController.value = 1f;
+        SFXVolumeController.value   = 1f;
+    }   
+
+    public bool IsActive(string name)
+    {   
+        GameObject ui = _uiDict[name];
+        return ui.activeSelf;
+    }   
+
+    public void EnableUI(string name)
+    {   
+        GameObject ui = _uiDict[name];
+        ui.SetActive(true);
+    }   
+
+    public void DisableUI(string name)
+    {   
+        GameObject ui = _uiDict[name];
+        ui.SetActive(false);
+    }   
+
+    public void ToggleUI(string name)
+    {   
+        GameObject ui = _uiDict[name];
+        ui.SetActive(!ui.activeSelf);
+
+        string state = ui.activeSelf ? "on" : "off";
+        this.Log($"{name} is {state}");
+    }   
+
+    public void MusicVolume()
+    {   
+        SoundManager.Instance.SetMusicVolume(MusicVolumeController.value);
+    }   
+
+    public void SFXVolume()
+    {   
+        SoundManager.Instance.SetSFXVolume(SFXVolumeController.value);
+    }   
 }
