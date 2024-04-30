7d35beb8e1258a781065414c4b36c5fffb73581a
Sanghwa Song <cnrrn421@gmail.com>
2024-04-30 18:44:42 +0900

    [Feature] NPC 를 지시하는 화살표 애니메이션 추가

---
+++ Assets/Scripts/Manager/DialogueManager/InteractSprite.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractSprite : MonoBehaviour
+{
+    [SerializeField] GameObject _interactableObject;
+
+    [SerializeField] private float _moveDistance;
+    [SerializeField] private float _moveSpeed;
+    
+    private float _angle;
+
+    private void Start()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        Vector3 origin = _interactableObject.transform.position;
+        Vector3 constant = Mathf.Abs(_interactableObject.transform.localScale.y) * Vector3.up;
+        Vector3 variable = _moveDistance * Mathf.Sin(_angle) * Vector3.up;
+
+        _angle += _moveSpeed * Time.deltaTime;
+
+        transform.position = origin + constant + variable;
+    }
+    
+
+    private void OnEnable() 
+    {
+        
+    }
+
+    private void OnDisable() 
+    {
+        _angle = 0f;   
+    }
+}
